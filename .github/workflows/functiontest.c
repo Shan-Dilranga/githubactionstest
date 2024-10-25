f0e5d1d8a961e985a5866bda80345a4b34837f46
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2024-10-26 03:40:18 +0530

    adding c function test file

---
+++ .github/workflows/functiontest.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int calsum(int x, int y){
+    return x+y;
+}
+
+int main(){
+
+    int resultarr[6], i;
+
+    resultarr[0]=calsum(2,5);
+    resultarr[1]=calsum(6,5);
+    resultarr[2]=calsum(2,13);
+    resultarr[3]=calsum(23,45);
+    resultarr[4]=calsum(21,54);
+    resultarr[5]=calsum(21,35);
+
+    for ( i = 0; i < sizeof(resultarr)/sizeof(resultarr[0]); i++)
+    {
+        printf("result no %d is : %d\n",i+1,resultarr[i] );
+    }
+    
+
+    return 0;
+}
